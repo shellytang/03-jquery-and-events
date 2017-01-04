fa0138bacc6922f934e418cf32cfcfb3e8bd5af1
Shelly Tang <shellytang@gmail.com>
2017-01-04 14:12:03 -0800

    completed to-dos on index and populateFilters and handleAuthors

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -19,15 +19,23 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
-      /* TODO: If the select box changes to an option that has a value, we should:
+      /* DONE: If the select box changes to an option that has a value, we should:
           1. Hide all of the articles
           2. Fade in only the articles that match based on on the author
             that was aselected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
+      $('article').hide();
+      var temp = $(this).val();
+      $('article').each(function(){
+        if($(this).attr('data-attribute') === temp) {
+          $(this).fadeIn();
+        }
+      });
     } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
+      $('article').not('.template').fadeIn();
     }
     $('#category-filter').val('');
   });
